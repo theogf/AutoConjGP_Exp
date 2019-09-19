d9b7f6797eabdbe0c65094809576b2e9a491f46d
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2019-09-19 12:40:02 +0200

    Added necessary packages

--- src/intro.jl
+++ src/intro.jl
@@ -1,7 +1,8 @@
 using Distributions, Random, Statistics
 using Random: seed!
-using DelimitedFiles, HDF5, BSON
+using DelimitedFiles, HDF5, BSON, Juno
 using MLDataUtils
+using PyCall
 using DataFrames
 using LinearAlgebra, Distances
 using AugmentedGaussianProcesses
