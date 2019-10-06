5800714b864abe37633d63ab19211022b7b9bc06
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-10-06 16:42:25 +0200

    Reworked exp dispatch

--- scripts/conv_exp.jl
+++ scripts/conv_exp.jl
@@ -9,8 +9,8 @@ defaultconvdict = Dict(:time_max=>1e4,:conv_max=>200,:file_name=>"covtype",
                         :nInducing=>50,:nMinibatch=>10,:likelihood=>:Logistic,
                         :doCAVI=>true,:doGD=>!true,:doNGD=>!true)
 
-convdictlist = Dict(:time_max=>1e4,:conv_max=>100,:file_name=>"heart",
-                        :nInducing=>[10,20,50],:nMinibatch=>[50,100,200],:likelihood=>:Logistic, :doCAVI=>true,:doGD=>true,:doNGD=>true)
+convdictlist = Dict(:time_max=>1e4,:conv_max=>50000,:file_name=>"CASP",
+                        :nInducing=>[100,200,500],:nMinibatch=>[50,100,200],:likelihood=>:Matern32, :doCAVI=>true,:doGD=>true,:doNGD=>true)
 convdictlist = dict_list(convdictlist)
 tmpfolder = projectdir("tmp","conv_exp",string(convdictlist[1][:file_name]),string(convdictlist[1][:likelihood]))
 # map(convergence_exp,convdictlist)
@@ -18,13 +18,14 @@ tmpfolder = projectdir("tmp","conv_exp",string(convdictlist[1][:file_name]),stri
 files = tmpsave(convdictlist,tmpfolder)
 try
     script = scriptdir("_conv_exp.jl")
-    for r in files
-        @show fullpath = "$(tmpfolder*r)"
-        submit = `screen -d -m julia -e $script $(fullpath)`# $(fullpath)`
+    for (i,r) in enumerate(files)
+        @show fullpath = joinpath(tmpfolder,r)
+        screenname = "Conv_"*string(convdictlist[i][:file_name])*"_"*string(convdictlist[i][:likelihood])*"_"*string(convdictlist[i][:nInducing])*"_"*string(convdictlist[i][:nMinibatch])
+        submit = `screen -d -S $screenname -m julia $script $(fullpath)`# $(fullpath)`
         run(submit)
     end
 catch e
-    rm(tmpfolder,recursive=true)
+    # rm(tmpfolder,recursive=true)
     rethrow(e)
 end
 # rm(tmpfolder,recursive=true)
